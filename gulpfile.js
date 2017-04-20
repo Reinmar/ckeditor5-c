9870423babe12a41faa09461a20a57bffe8d8ba5
Kamil Piechaczek <pomek@pomek.pl>
2017-04-20 10:33:29 +0200

    Fix: Adjusted API to the latest changes.

--- gulpfile.js
+++ gulpfile.js
@@ -4,5 +4,5 @@ const gulp = require( 'gulp' );
 
 // Generate changelog for the current package.
 gulp.task( 'changelog', () => {
-	return require( '@ckeditor/ckeditor5-dev-env' ).generateChangelog();
+	return require( '@ckeditor/ckeditor5-dev-env' ).generateChangelogForSinglePackage();
 } );
